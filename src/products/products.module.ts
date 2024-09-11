982e5aaca705e9906de1d86d02a2224e322a0cf5
PabloAndres2000 <l.pablo4125@gmail.com>
2024-09-11 00:13:04 -0300

    se ingresa nueva infraestructura de mensajeria NATS

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs, PRODUCT_SERVICE } from 'src/config';
-import { OrdersModule } from 'src/orders/orders.module';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
   imports: [
-    ClientsModule.register([
-      { 
-        name: PRODUCT_SERVICE, 
-        transport: Transport.TCP,
-        options: {
-          host: envs.productsMicroservice.host,  
-          port: envs.productsMicroservice.port  
-        }
-      },
-    ]),
+    NatsModule
   ]
 })
 export class ProductsModule {}
